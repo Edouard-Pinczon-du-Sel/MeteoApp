c8de285cdfa97363b41a5b318ad49cdce448d5d4
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-10-15 23:01:05 +0200

    update : ask request api weather and catch errors

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ if(navigator.geolocation) {
         loader.textContent = "Vous avez refuser la géolocalisation, veuillez l'activer."
     })
 }
+
+async function getWeatherData(long, lat) {
+    try {
+        const results = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=a783ff1b655a8efef32760dc98437171`)
+        
+        if(!results.ok) {
+            // on créer une erreur
+            throw new Error(`Erreur: ${results.status}`)
+        }
+        const data = await results.json();
+        console.log(data);
+        // on enleve le loader
+        loader.classList.add("fade-out");
+    }
+
+    catch(e) {
+        loader.textContent = e;
+    }
+}
