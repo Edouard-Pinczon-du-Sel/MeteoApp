50d5160fc5d80261b85b7321e80693a7c2c2f2d0
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2022-10-25 13:57:04 +0200

    add prevision of temperatures to hours

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ async function getWeatherData(long, lat) {
         }
         const data = await results.json();
         console.log(data);
+        populateMAinInfo(data);
+        handleHours(data.hourly);
         // on enleve le loader
         loader.classList.add("fade-out");
     }
@@ -29,3 +31,45 @@ async function getWeatherData(long, lat) {
         loader.textContent = e;
     }
 }
+
+const position = document.querySelector(".position");
+const temperature = document.querySelector(".temperature");
+const weatherImage = document.querySelector(".weather-image");
+
+// prendre la date actuelle
+const currentHour = new Date().getHours()
+function populateMAinInfo(data) {
+    // on va chercher dans l'objet data la temperature
+    temperature.textContent = `${Math.trunc(data.current.temp)}°`
+    position.textContent = data.timezone;
+
+    if(currentHour >= 6 && currentHour < 21) {
+        weatherImage.src = `ressources/jour/${data.current.weather[0].icon}.svg`
+    }else{
+        weatherImage.src = `ressources/nuit/${data.current.weather[0].icon}.svg`
+    }
+
+}
+const hourNameBlocks = document.querySelectorAll(".hour-name")
+const hourTemperatures = document.querySelectorAll(".hour-temp")
+
+function handleHours(data) {
+    hourNameBlocks.forEach((block, index) => {
+        // pour changer l'heure de +3 heures
+        const incrementedHour = currentHour + index*3
+
+        if(incrementedHour > 24) {
+            const calcul = incrementedHour - 24;
+            hourNameBlocks[index].textContent = `${calcul === 24 ? "00" : calcul}h`
+        } else if(incrementedHour === 24) {
+            hourNameBlocks.textContent = "00h";
+        }else {
+            hourNameBlocks[index].textContent = `${incrementedHour}h`
+        }
+        // temperatures
+        hourTemperatures[index].textContent = `${Math.trunc(data[index * 3].temp)}°`
+
+
+    })
+}
+
